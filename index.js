03e054589cda022d96807c92b241cc32039dea36
Matthew Chase Whittemore <matthew@socialtables.com>
2013-10-31 00:48:18 -0400

    working on type creation and trying to get an endpoint up

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
+var querystring = require("querystring");
 var connect = require("connect");
 var version = require("./lib/version");
 
 
-module.exports = function(opts) {
+module.exports = function(apiOpts) {
 
-    process.NODE_ENV = process.NODE_ENV || "dev";
+    process.NODE_ENV = process.NODE_ENV || 'development';
 
-    opts = opts || {};
-    opts.publicFolder = opts.publicFolder || 'public';
-    opts.logEnv = opts.logEnv || 'dev';
-    opts.docsPath = opts.docsPath || 'docs';
+    apiOpts = apiOpts || {};
+    apiOpts.publicFolder = apiOpts.publicFolder || 'public';
+    apiOpts.logEnv = apiOpts.logEnv || 'development';
+    apiOpts.docsPath = apiOpts.docsPath || 'docs';
+    if (apiOpts.docsPath == "api") {
+        throw new Error("DOCSPATH CANNOT BE API");
+    }
 
     var api = connect();
     api.use(connect.bodyParser());
     api.use(connect.cookieParser());
-
-    api.use(connect.logger(opts.logEnv));
-    api.use(connect.static(opts.publicFolder));
+    api.use(connect.static(apiOpts.publicFolder));
 
     //adding in express' res.json function
     api.use(require("./lib/express-response"));
 
     var versions = [];
-    var versionNameMap = {};
+    var versionNumberMappedToName = {};
 
     api.use(function(req, res, next) {
-        // 1. Look for api/docs
-        // 2. Push along
-        next();
+
+        var urlParts = req.url.split("?");
+
+        var path = urlParts[0];
+        var parts = path.split("/").slice(1);
+
+        req.query = urlParts[1] ? querystring.parse(urlParts[1]) : {};
+
+        if (typeof versionNumberMappedToName[parts[0]] != "undefined") {
+            var routeName = "api/" + req.method.toLowerCase() + "/" + parts.slice(1).join("/");
+            var versionId = versionNumberMappedToName[parts[0]];
+            var version = versions[versionId];
+            var acceptedRoute = version.router(routeName, req, res, next);
+        } else if (parts[0] == apiOpts.docsPath && typeof versionNumberMappedToName[parts[1]] != "undefined") {
+            var routeName = apiOpts.docsPath + "/" + parts.slice(2).join("/");
+            var versionId = versionNumberMappedToName[parts[1]];
+            var version = versions[versionId];
+            var acceptedRoute = version.router(routeName, req, res, next);
+        } else {
+            next();
+        }
+
     });
 
-    api.version = function(opts) {
+    api.version = function(verOpts) {
+        verOpts.docsPath = apiOpts.docsPath;
         var parent = versions.length != 0 ? versions[versions.length - 1] : null;
-        var ver = version(api, parent, opts);
+        var ver = version(api, parent, verOpts);
         versions.push(ver);
-        versionNameMap[ver.name] = versions.length - 1;
+        versionNumberMappedToName[ver.name] = versions.length - 1;
         return ver;
     }
 
--- test/test-app/endpoints/0.1.js
+++ test/test-app/endpoints/0.1.js
@@ -1,3 +1,13 @@
 module.exports = function(ver) {
 
+    ver.get("test", {
+        request: "empty",
+        response: "object",
+        desc: "just a test"
+    }, function(req, res) {
+        res.json({
+            "woot": "woot"
+        });
+    });
+
 }
