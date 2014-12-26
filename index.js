93a4676e688554bf12bd8a1e8637e34b1eca4221
Matthew Chase Whittemore <matthew@socialtables.com>
2014-12-25 23:21:02 -0500

    refactor versions into object with version name as the key

--- index.js
+++ index.js
@@ -9,21 +9,26 @@ module.exports = function(){
     var opi = {};
     opi.listen = app.listen.bind(app);
 
-    var versions = [];
+    var versions = {};
+    var versionNames = [];
 
     opi.version = function(name){
-        var version = Version(name);
-        versions.push(version);
+        var version = Version();
+        versions[name] = version.routes;
+        versionNames.push(name);
         return version.endUser;
     }
 
     setTimeout(function(){
         merger(versions);
         
-        for(var i=0; i<versions.length; i++){
-            var routes = Object.keys(versions[i].routes);
-            for(var j=0; j<routes.length; j++){
-                var route = versions[i].routes[routes[j]];
+        for(var i=0; i<versionNames.length; i++){
+            var versionName = versionNames[i];
+            var version = versions[versionName];
+            var routeNames = Object.keys(version);
+            for(var j=0; j<routeNames.length; j++){
+                var routeName = routeNames[j];
+                var route = version[routeName];
 
                 var routeMethods = Object.keys(route);
 
@@ -31,14 +36,10 @@ module.exports = function(){
                     var routeMethod = routeMethods[k];
                     var endPoint = route[routeMethod];
 
-                    var routeVers = endPoint.versions;
-                    for(var l=0; l<routeVers.length; l++){
-                        var ver = routeVers[l];
-                        var path = "/"+ver+endPoint.path;
-                        var args = [path].concat(endPoint.fns);
+                    var path = "/"+versionName+endPoint.path;
+                    var args = [path].concat(endPoint.fns);
 
-                        app[routeMethod].apply(app, args);
-                    }
+                    app[routeMethod].apply(app, args);
                 }
 
             }
