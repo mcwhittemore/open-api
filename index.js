1ffd840d9080d691e9455090e29d608de14539e0
Matthew Chase Whittemore <matthew@socialtables.com>
2014-12-25 23:52:21 -0500

    json version of the docs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var merger = require("./lib/merge-versions");
 var express = require("express");
 
 
-module.exports = function(){
+module.exports = function(apiTitle){
     var app = express();
 
     var opi = {};
@@ -22,28 +22,60 @@ module.exports = function(){
     setTimeout(function(){
         merger(versions);
         
-        for(var i=0; i<versionNames.length; i++){
-            var versionName = versionNames[i];
+        versionNames.forEach(function(versionName){
             var version = versions[versionName];
             var routeNames = Object.keys(version);
-            for(var j=0; j<routeNames.length; j++){
-                var routeName = routeNames[j];
+
+            var docRoutes = [];
+
+            routeNames.forEach(function(routeName){
                 var route = version[routeName];
+
+                var path = "/"+versionName+routeName;
+                var docsPath = "/docs"+path;
+
+                docRoutes.push({
+                    name: routeName,
+                    url: docsPath,
+                    isCurrentPage: false
+                });
+
+                var docMethods = {};
+
+                app.get(docsPath, function(req, res){
+                    var docs = {};
+                    docs.routes = docRoutes;
+                    docs.versions = versionNames;
+                    docs.apiTitle = apiTitle;
+                    docs.versionTitle = versionName;
+                    docs.routeTitle = routeName;
+                    docs.pageTitle = path;
+                    docs.methods = docMethods;
+
+                    res.json(docs);
+                });
 
                 var routeMethods = Object.keys(route);
 
-                for(var k=0; k<routeMethods.length; k++){
-                    var routeMethod = routeMethods[k];
+                routeMethods.sort(function(a, b) {
+                    return a.localeCompare(b);
+                });
+
+                routeMethods.forEach(function(routeMethod){
                     var endPoint = route[routeMethod];
 
-                    var path = "/"+versionName+endPoint.path;
+                    docMethods[routeMethod] = endPoint.docs.join("\n");
+
                     var args = [path].concat(endPoint.fns);
 
                     app[routeMethod].apply(app, args);
-                }
+                });
+            });
 
-            }
-        }
+            docRoutes.sort(function(a, b) {
+                return a.name.localeCompare(b.name);
+            });
+        });
 
     },0);
 
