1d97c1dd52bcf52dd47578b1a0e8351bb24238e9
Matthew Chase Whittemore <matthew@socialtables.com>
2014-12-26 00:24:42 -0500

    docs running for all pages, needs markdown and stream support

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var Version = require("./lib/version");
 var versionMerger = require("./lib/merge-versions");
 var appBuilder = require("./lib/app-builder");
 var express = require("express");
+var ejsListRender = require("ejs-list-render");
 
 
 module.exports = function(apiTitle){
     var app = express();
+
+    app.use(ejsListRender.connect);
 
     var opi = {};
     opi.listen = app.listen.bind(app);
@@ -14,8 +17,8 @@ module.exports = function(apiTitle){
     var versionNames = [];
 
     opi.version = function(name){
-        var version = Version();
-        versions[name] = version.routes;
+        var version = Version(name);
+        versions[name] = version;
         versionNames.push(name);
         return version.endUser;
     }
