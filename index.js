c42e6d59bdb777e825d151baac17af94fdd9e5a5
Matthew Chase Whittemore <matthew@socialtables.com>
2014-12-26 00:04:15 -0500

    breaking things into their own files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Version = require("./lib/version");
-var merger = require("./lib/merge-versions");
+var versionMerger = require("./lib/merge-versions");
+var appBuilder = require("./lib/app-builder");
 var express = require("express");
 
 
@@ -20,63 +21,8 @@ module.exports = function(apiTitle){
     }
 
     setTimeout(function(){
-        merger(versions);
-        
-        versionNames.forEach(function(versionName){
-            var version = versions[versionName];
-            var routeNames = Object.keys(version);
-
-            var docRoutes = [];
-
-            routeNames.forEach(function(routeName){
-                var route = version[routeName];
-
-                var path = "/"+versionName+routeName;
-                var docsPath = "/docs"+path;
-
-                docRoutes.push({
-                    name: routeName,
-                    url: docsPath,
-                    isCurrentPage: false
-                });
-
-                var docMethods = {};
-
-                app.get(docsPath, function(req, res){
-                    var docs = {};
-                    docs.routes = docRoutes;
-                    docs.versions = versionNames;
-                    docs.apiTitle = apiTitle;
-                    docs.versionTitle = versionName;
-                    docs.routeTitle = routeName;
-                    docs.pageTitle = path;
-                    docs.methods = docMethods;
-
-                    res.json(docs);
-                });
-
-                var routeMethods = Object.keys(route);
-
-                routeMethods.sort(function(a, b) {
-                    return a.localeCompare(b);
-                });
-
-                routeMethods.forEach(function(routeMethod){
-                    var endPoint = route[routeMethod];
-
-                    docMethods[routeMethod] = endPoint.docs.join("\n");
-
-                    var args = [path].concat(endPoint.fns);
-
-                    app[routeMethod].apply(app, args);
-                });
-            });
-
-            docRoutes.sort(function(a, b) {
-                return a.name.localeCompare(b.name);
-            });
-        });
-
+        versionMerger(versions);
+        appBuilder(app, versions, versionNames, apiTitle);
     },0);
 
     return opi;
